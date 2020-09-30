260d353229d3011c939c44bc747e0a523dd11682
robsta7 <meremeev2001@gmail.com>
2020-09-30 11:32:30 +0300

    Начал MainTabBarController

---
+++ MissingPet/App/Presentation/Main/Controllers/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 30.09.2020.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
