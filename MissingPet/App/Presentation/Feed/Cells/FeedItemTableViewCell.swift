da302f84ee6b36f8e1bde6be4b64c2da979359f3
robsta7 <meremeev2001@gmail.com>
2020-10-14 22:45:28 +0300

    Добавлена ячейка для ленты объявлений

---
+++ MissingPet/App/Presentation/Feed/Cells/FeedItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedItemTableViewCell.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 01.10.2020.
+//
+
+import UIKit
+
+class FeedItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- MissingPet/App/Presentation/Feed/Controllers/FeedViewController.swift
+++ MissingPet/App/Presentation/Feed/Controllers/FeedViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class FeedViewController: Controller<FeedPresenter> {
     
+    @IBOutlet weak var feedTableView: UITableView!
     override func viewDidLoad() {
         
+        
         super.viewDidLoad()
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
+        feedTableView.register(UINib(nibName: "FeedItemTableViewCell", bundle: nil), forCellReuseIdentifier: "FeedItemTableViewCell")
         
     }
+    
+    
+}
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let feedCell = feedTableView.dequeueReusableCell(withIdentifier: "FeedItemTableViewCell", for: indexPath) as! FeedItemTableViewCell
+        feedCell.separatorInset.bottom = .infinity
+        feedCell.separatorInset.left = .infinity
+        feedCell.separatorInset.top = .infinity
+        feedCell.separatorInset.right = .infinity
+        return feedCell
+    }
+    
+    
     
     
 }
