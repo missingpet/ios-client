cf625adcf5a3ec25179350b9aea56a02acb1a390
robsta7 <meremeev2001@gmail.com>
2020-10-19 01:53:43 +0300

    Добавлен NewAdPresenter

---
+++ MissingPet/App/Presentation/NewAd/Presenters/NewAdPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  NewAdPresenter.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 19.10.2020.
+//
+
+import Foundation
+
+class NewAdPresenter: DefaultPresenterType {
+
+    required init() {
+        
+    }
+    
+}
