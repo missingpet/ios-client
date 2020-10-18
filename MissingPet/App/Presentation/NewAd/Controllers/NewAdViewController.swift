bdd705c79f0fdabf178ab00778f5e65d55fc5bec
robsta7 <meremeev2001@gmail.com>
2020-10-19 01:54:27 +0300

    Добавлен класс экрана NewAdViewController

---
+++ MissingPet/App/Presentation/NewAd/Controllers/NewAdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewAdViewController.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 19.10.2020.
+//
+
+import UIKit
+
+class NewAdViewController: Controller<NewAdPresenter> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
