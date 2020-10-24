65d9ba666f83fca4f82eb066bd44365d875d8bcc
robsta7 <meremeev2001@gmail.com>
2020-10-24 18:25:52 +0300

    Added custom colors enum

---
+++ MissingPet/Resources/Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Colors.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 24.10.2020.
+//
+
+import Foundation
+import UIKit
+
+enum CustomColor: String {
+    case mainWhite = "MainWhite"
+    case lightGray = "LightGray"
+    case mediumGray = "MediumGray"
+    case darkGray = "DarkGray"
+    case lightBlue = "LightBlue"
+    case blue = "Blue"
+    case darkBlue = "DarkBlue"
+}
+
+extension UIColor {
+    static func custom(_ color: CustomColor) -> UIColor {
+        return UIColor(named: color.rawValue)!
+    }
+}
