1f90d81b7f0ab5164519313b8a716fa5c98439ab
robsta7 <meremeev2001@gmail.com>
2020-10-29 23:22:51 +0300

    Улучшил ячейку для объявления

--- MissingPet/ReusableUI/Cells/AnnouncementTableViewCell.swift
+++ MissingPet/ReusableUI/Cells/AnnouncementTableViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class AnnouncementTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var announcementImageView: AnnouncementImageView!
+    @IBOutlet weak var creationDateLabel: UILabel!
+    @IBOutlet weak var descriprionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -16,8 +20,28 @@ class AnnouncementTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupInsets()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupInsets()
+    }
+    
+}
+
+extension AnnouncementTableViewCell {
+    
+    func setupInsets() {
+        separatorInset.bottom = .infinity
+        separatorInset.left = .infinity
+        separatorInset.top = .infinity
+        separatorInset.right = .infinity
     }
     
 }
