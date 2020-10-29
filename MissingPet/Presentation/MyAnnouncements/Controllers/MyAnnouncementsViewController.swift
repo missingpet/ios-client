c6a6522f75fc2801fe2e9c08dd6505949d518490
robsta7 <meremeev2001@gmail.com>
2020-10-29 23:23:49 +0300

    Обновлён класс контроллера объявлений пользователя

--- MissingPet/Presentation/MyAnnouncements/Controllers/MyAnnouncementsViewController.swift
+++ MissingPet/Presentation/MyAnnouncements/Controllers/MyAnnouncementsViewController.swift
@@ -16,6 +16,10 @@ class MyAnnouncementsViewController: Controller<MyAnnouncementsPresenter> {
         setupMyAnnouncementsTableView()
     }
     
+}
+
+extension MyAnnouncementsViewController {
+    
     func setupMyAnnouncementsTableView() {
         myAnnouncementsTableView.delegate = self
         myAnnouncementsTableView.dataSource = self
@@ -24,7 +28,13 @@ class MyAnnouncementsViewController: Controller<MyAnnouncementsPresenter> {
     
 }
 
-extension MyAnnouncementsViewController: UITableViewDelegate, UITableViewDataSource {
+extension MyAnnouncementsViewController: UITableViewDelegate {
+    
+    
+    
+}
+
+extension MyAnnouncementsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         10
@@ -32,10 +42,9 @@ extension MyAnnouncementsViewController: UITableViewDelegate, UITableViewDataSou
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let announcementCell = myAnnouncementsTableView.dequeueReusableCell(withIdentifier: "AnnouncementTableViewCell", for: indexPath) as! AnnouncementTableViewCell
-        announcementCell.separatorInset.bottom = .infinity
-        announcementCell.separatorInset.left = .infinity
-        announcementCell.separatorInset.top = .infinity
-        announcementCell.separatorInset.right = .infinity
+        announcementCell.announcementImageView.image = UIImage(named: "announcement-template-1")
+        announcementCell.creationDateLabel.text = "27 октября 2020, 12:04"
+        announcementCell.descriprionLabel.text = "Потерялась собака по кличке Арчи. Порода - доберман. Черного цвета."
         return announcementCell
     }
     
