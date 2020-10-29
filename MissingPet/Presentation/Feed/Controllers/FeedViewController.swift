b5f852483f1b818326a72d702fcd212a9238328b
robsta7 <meremeev2001@gmail.com>
2020-10-29 23:24:19 +0300

    Обновлён класс контроллера ленты

--- MissingPet/Presentation/Feed/Controllers/FeedViewController.swift
+++ MissingPet/Presentation/Feed/Controllers/FeedViewController.swift
@@ -16,16 +16,23 @@ class FeedViewController: Controller<FeedPresenter> {
         setupFeedTableView()
     }
     
+}
+
+extension FeedViewController {
+    
     func setupFeedTableView() {
         feedTableView.delegate = self
         feedTableView.dataSource = self
         feedTableView.register(UINib(nibName: "AnnouncementTableViewCell", bundle: nil), forCellReuseIdentifier: "AnnouncementTableViewCell")
     }
     
+}
+
+extension FeedViewController: UITableViewDelegate {
     
 }
 
-extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+extension FeedViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         20
@@ -33,10 +40,9 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let feedCell = feedTableView.dequeueReusableCell(withIdentifier: "AnnouncementTableViewCell", for: indexPath) as! AnnouncementTableViewCell
-        feedCell.separatorInset.bottom = .infinity
-        feedCell.separatorInset.left = .infinity
-        feedCell.separatorInset.top = .infinity
-        feedCell.separatorInset.right = .infinity
+        feedCell.announcementImageView.image = UIImage(named: "announcement-template-0")
+        feedCell.creationDateLabel.text = "21 октября 2020, 15:34"
+        feedCell.descriprionLabel.text = "Пропал пёс по кличке Ричард. Сорвался с поводка в городе Ростове-на-Дону. На нем был ошейник черного цвета. Прошу вернуть за вознаграждение!"
         return feedCell
     }
     
