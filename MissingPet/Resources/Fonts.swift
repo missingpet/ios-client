f6c313e0dc5ce1066bb38ad38ddefb3dc13e38dd
robsta7 <meremeev2001@gmail.com>
2020-11-07 01:49:56 +0300

    Расширен класс UIFont

---
+++ MissingPet/Resources/Fonts.swift
@@ -0,0 +1,23 @@
+//
+//  Fonts.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 03.11.2020.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    
+    static let defaultFontSize: CGFloat = 14.0
+    
+    enum CustomName: String {
+        case sfUiDisplayBold = "SFUIDisplay-Bold"
+    }
+    
+    static func custom(_ customName: CustomName, size: CGFloat) -> UIFont {
+        return UIFont(name: customName.rawValue, size: size)!
+    }
+    
+}
