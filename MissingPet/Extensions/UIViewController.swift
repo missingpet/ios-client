39af5a0d8e58cc100fbce8042de3e534d09a3c5b
robsta7 <meremeev2001@gmail.com>
2020-11-29 01:38:08 +0300

    Расширен UIViewController

---
+++ MissingPet/Extensions/UIViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 29.11.2020.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardOnTap() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+}
