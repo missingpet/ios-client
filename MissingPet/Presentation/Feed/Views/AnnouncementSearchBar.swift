34bf6c161ca2ec41c4ffef0ca7900a6c7710ba62
robsta7 <meremeev2001@gmail.com>
2020-11-28 11:31:02 +0300

    Удалён AnnouncementSearchBar

---
+++ MissingPet/Presentation/Feed/Views/AnnouncementSearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  AnnouncementSearchBar.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 31.10.2020.
+//
+
+import UIKit
+
+class AnnouncementSearchBar: UISearchBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
