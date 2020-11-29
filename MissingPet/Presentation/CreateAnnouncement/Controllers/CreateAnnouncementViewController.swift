9ba040e2e5cc284c7f9b7f6bfb685c1dea59dc95
robsta7 <meremeev2001@gmail.com>
2020-11-29 13:42:00 +0300

    Добавил аутлеты announcementTypeView, animalTypeView, placeView

--- MissingPet/Presentation/CreateAnnouncement/Controllers/CreateAnnouncementViewController.swift
+++ MissingPet/Presentation/CreateAnnouncement/Controllers/CreateAnnouncementViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class CreateAnnouncementViewController: Controller<CreateAnnouncementPresenter> {
 
+    @IBOutlet weak var announcementTypeView: InfoView!
+    @IBOutlet weak var animalTypeView: InfoView!
+    @IBOutlet weak var placeView: InfoView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardOnTap()
     }
-    
     
     @IBAction func presentConfirmCreateAlert(_ sender: UIButton) {
         presenter?.presentConfirmCreateAlert(viewController: self)
