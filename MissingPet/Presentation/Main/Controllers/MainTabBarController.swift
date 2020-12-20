09cc6505371c7b90f3485c0938123cbc1642db48
robsta7 <meremeev2001@gmail.com>
2020-12-21 01:46:52 +0300

    Update arch (MainTabBarController)

--- MissingPet/Presentation/Main/Controllers/MainTabBarController.swift
+++ MissingPet/Presentation/Main/Controllers/MainTabBarController.swift
@@ -13,20 +13,20 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
         
         viewControllers = [
-            createController(FeedViewController.self, in: FeedNavigationController.self, from: .feed, iconName: "feed"),
-            createController(MapViewController.self, in: MapNavigationController.self, from: .map, iconName: "map"),
-            createController(CreateAnnouncementViewController.self, in: CreateAnnouncementNavigationController.self, from: .createAnnouncement, iconName: "create-announcement"),
-            createController(MyAnnouncementsViewController.self, in: MyAnnouncementsNavigationController.self, from: .myAnnouncements, iconName: "my-announcements"),
-            //createController(ProfileViewController.self, in: ProfileNavigationController.self, from: .profile, iconName: "profile"),
-            createController(SignInViewController.self, in: SignInNavigationController.self, from: .signIn, iconName: "profile"),
+            createController(FeedViewController.self, with: FeedPresenter(), in: FeedNavigationController.self, from: .feed, iconName: "feed"),
+            createController(MapViewController.self, with: MapPresenter(), in: MapNavigationController.self, from: .map, iconName: "map"),
+            createController(CreateAnnouncementViewController.self, with: CreateAnnouncementPresenter(), in: CreateAnnouncementNavigationController.self, from: .createAnnouncement, iconName: "create-announcement"),
+            createController(MyAnnouncementsViewController.self, with: MyAnnouncementsPresenter(),in: MyAnnouncementsNavigationController.self, from: .myAnnouncements, iconName: "my-announcements"),
+            //createController(ProfileViewController.self, with: ProfilePresenter(), in: ProfileNavigationController.self, from: .profile, iconName: "profile"),
+            createController(SignInViewController.self, with: SignInPresenter(), in: SignInNavigationController.self, from: .signIn, iconName: "profile"),
         ]
 
     }
     
-    private func createController<P: DefaultPresenterType, C: Controller<P>, NC: UINavigationController>(_ controller: C.Type, in navigationController: NC.Type, from storyboard: Storyboard, iconName: String) -> UINavigationController {
+    private func createController<P: PresenterType, C: Controller<P>, NC: UINavigationController>(_ controller: C.Type, with presenter: P,in navigationController: NC.Type, from storyboard: Storyboard, iconName: String) -> UINavigationController {
         let navigator = Navigator(storyboard)
         let navigationController = navigator.createController(NC.self)
-        let controller: C = navigator.createControllerWithDefaultPresenter(C.self)
+        let controller: C = navigator.createControllerWithPresenter(C.self, presenter: presenter)
         navigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: iconName)?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: iconName + "-selected")?.withRenderingMode(.alwaysOriginal))
         navigationController.viewControllers = [controller]
         return navigationController
