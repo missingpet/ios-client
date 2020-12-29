598aebb2e4378039024048dc58f8f8e22e38e5d8
robsta7 <meremeev2001@gmail.com>
2020-12-30 02:48:37 +0300

    Start PhotoView

---
+++ MissingPet/Presentation/CreateAnnouncement/Views/PhotoView/PhotoView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoView.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 28.12.2020.
+//
+
+import UIKit
+
+class PhotoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
