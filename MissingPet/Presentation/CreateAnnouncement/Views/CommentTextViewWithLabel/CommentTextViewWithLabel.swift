6e90e8ff9218fe3aaf3ab3b0ac815a1f2a8a1b41
robsta7 <meremeev2001@gmail.com>
2020-12-30 02:48:09 +0300

    Add CommentTextViewWithLabel

---
+++ MissingPet/Presentation/CreateAnnouncement/Views/CommentTextViewWithLabel/CommentTextViewWithLabel.swift
@@ -0,0 +1,45 @@
+//
+//  CommentTextViewWithLabel.swift
+//  MissingPet
+//
+//  Created by Михаил Еремеев on 28.12.2020.
+//
+
+import UIKit
+
+@IBDesignable
+class CommentTextViewWithLabel: UIView {
+
+    @IBOutlet var view: UIView!
+    
+    @IBOutlet weak var shadowView: ShadowView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadUI()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        loadUI()
+    }
+    
+    private func loadUI() {
+        
+        view = loadNibView()
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        view.frame = bounds
+        addSubview(view)
+        
+        textView.text = nil
+        
+    }
+    
+}
