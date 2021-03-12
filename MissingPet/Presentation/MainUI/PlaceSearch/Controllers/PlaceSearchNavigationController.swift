f7a372f23943d1337121824493851f82baa9a4ee
robsta7 <meremeev2001@gmail.com>
2021-03-12 23:42:25 +0300

    Update(PlaceSearch): add PlaceSearchNavigationController

---
+++ MissingPet/Presentation/MainUI/PlaceSearch/Controllers/PlaceSearchNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceSearchNavigationController.swift
+//  MissingPet
+//
+//  Created by Mikhail Eremeev on 12.03.2021.
+//
+
+import Foundation
+
+class PlaceSearchNavigationController: NavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
